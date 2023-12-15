4204eb3362b794f77d9091bcab9b01457ba1e0da
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2023-12-15 03:35:18 +0300

    kodu sadeleştirdik
    okunurluğun artması için sadeleştirdik

--- TekSayilar.java
+++ TekSayilar.java
@@ -7,11 +7,8 @@ public class TekSayilar {
         do {
             System.out.print("Sayı giriniz:  ");
             number = input.nextInt();
-            if(number % 2 != 0 && number > 0) sum += number;
+            if(number % 2 != 0) sum += number;
         }while(number > 0);
-        // if i kullanmamızın nedeni
-        // sum değerine eksi değer olmasına rağmen toplama ekliyor son girilen eksi değeri
-        // ama if koyarsak sadece pozitif sayıları ekliyor sum değerine
 
 System.out.print(sum);
 
